9ebee3952f2c9dcf905fb80fea686d6f7b3d0208
Mannelig92 <mannelig92@gmail.com>
2021-08-15 16:08:25 +0300

    v1.1

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,15 +5,15 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class server {
+public class Server {
     public static void main(String[] args) {
         int port = 8080;
-        try (ServerSocket serverSocket = new ServerSocket(port);
-             Socket clientSocket = serverSocket.accept(); // ждем подключения
+        try (ServerSocket server = new ServerSocket(port); //+
+             Socket clientSocket = server.accept(); // ждем подключения
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             System.out.println("New connection accepted");
-            final String name = in.readLine();
+            String name = in.readLine();
             out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
         } catch (IOException e) {
             System.out.println(e.getMessage());
